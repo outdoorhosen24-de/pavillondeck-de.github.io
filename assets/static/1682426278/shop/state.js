9d3b04e1672253268b2be3fdb349b63e637612b8
outdoorhosen24-de <outdoorhosen24-de@users.noreply.github.com>
2023-04-26 16:29:27 +0000

    deploy: 2b0b013b4df2d8756905a9e0f093f38582a7db4b

--- assets/static/1682426278/shop/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fassets\u002Fstatic\u002F1682426278",layout:"default",error:a,serverRendered:true,routePath:"\u002Fshop\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fassets\u002F",cdnURL:a}}}}(null));
